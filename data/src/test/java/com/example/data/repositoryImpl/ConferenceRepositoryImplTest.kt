f23cf83cb94a7a11aefc448591cdfe6a538dad64
evan.969 <evan.969@kakaocorp.com>
2022-01-24 22:50:44 +0900

    use mock data to test

--- data/src/test/java/com/example/data/repositoryImpl/ConferenceRepositoryImplTest.kt
+++ data/src/test/java/com/example/data/repositoryImpl/ConferenceRepositoryImplTest.kt
@@ -1,5 +1,6 @@
 package com.example.data.repositoryImpl
 
+import com.example.data.MockData
 import com.example.data.network.IfKakaoService
 import kotlinx.coroutines.runBlocking
 import org.junit.Assert.*
@@ -7,6 +8,7 @@ import org.junit.Assert.*
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
+import org.mockito.Mockito
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -17,11 +19,19 @@ class ConferenceRepositoryImplTest {
         .baseUrl("https://raw.githubusercontent.com")
         .build()
 
-    private val testService =
+
+    private val realMockRepository =
         ConferenceRepositoryImpl(testRetrofit.create(IfKakaoService::class.java))
+
+    val fakeMockitoRepository = Mockito.mock(ConferenceRepositoryImpl::class.java)
 
     @Before
     fun setUp() {
+        runBlocking {
+            Mockito.`when`(fakeMockitoRepository.getConferences()).thenReturn(
+                MockData.conference
+            )
+        }
     }
 
     @After
@@ -29,8 +39,22 @@ class ConferenceRepositoryImplTest {
     }
 
     @Test
-    fun getConferences() = runBlocking {
-        val result = testService.getConferences()
+    fun getConferencesFromAPI() = runBlocking {
+        val result = realMockRepository.getConferences()
         assertEquals(120, result.data.size)
+        assertEquals(true, result.success)
+        assertEquals("카카오엔터프라이즈가 그려가고 있는  Enterprise IT", result.data[4].title)
+        assertEquals("모두를 위한 서비스, 카카오가 만들어가는 세상", result.data[10].title)
+        assertEquals("카카오 애자일 상담소 (3일차)", result.data[result.data.size - 1].title)
+    }
+
+    @Test
+    fun getConferencesFromMock() = runBlocking {
+        val result = fakeMockitoRepository.getConferences()
+        assertEquals(3, result.data.size)
+        assertEquals(true, result.success)
+        assertEquals("모두를 위한 서비스, 카카오가 만들어가는 세상", result.data[0].title)
+        assertEquals("카카오임팩트 펠로우십을 통해 본  사회 혁신가의 성장 요인", result.data[1].title)
+        assertEquals("카카오워크가 그리는 일하는 방식의 미래:  카카오워크 안에서 이뤄지는 업무의 완결", result.data[2].title)
     }
 }
