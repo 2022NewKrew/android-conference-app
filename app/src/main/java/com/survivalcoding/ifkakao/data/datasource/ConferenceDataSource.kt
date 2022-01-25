6d0ec13c40810f79d17bb0fa6b724b26d96cf062
gongwho <hklee4000@gmail.com>
2022-01-26 01:07:36 +0900

    transfer datasource test status red -> green

--- app/src/main/java/com/survivalcoding/ifkakao/data/datasource/ConferenceDataSource.kt
+++ app/src/main/java/com/survivalcoding/ifkakao/data/datasource/ConferenceDataSource.kt
@@ -1,8 +1,40 @@
 package com.survivalcoding.ifkakao.data.datasource
 
+import com.survivalcoding.ifkakao.domain.model.ApiDto
+import com.survivalcoding.ifkakao.domain.repository.NetworkResult
+import retrofit2.Response
 import javax.inject.Inject
 
-class ConferenceDataSource @Inject constructor(conferenceService: ConferenceService) {
-    suspend fun getData() {
+class ConferenceDataSource @Inject constructor(private val conferenceService: ConferenceService) {
+    suspend fun getData(): ApiDto? {
+        return when (
+            val response =
+                safeApiCall { conferenceService.getData() }
+        ) {
+            is NetworkResult.Success -> {
+                response.data
+            }
+            is NetworkResult.Error -> {
+                null
+            }
+        }
     }
+
+    private suspend fun <T> safeApiCall(apiCall: suspend () -> Response<T>): NetworkResult<T> {
+        try {
+            val response = apiCall()
+            if (response.isSuccessful) {
+                val body = response.body()
+                body?.let {
+                    return NetworkResult.Success(body)
+                }
+            }
+            return error("${response.code()} ${response.message()}")
+        } catch (e: Exception) {
+            return error(e.message ?: e.toString())
+        }
+    }
+
+    private fun <T> error(errorMessage: String): NetworkResult<T> =
+        NetworkResult.Error("Api call failed $errorMessage")
 }
