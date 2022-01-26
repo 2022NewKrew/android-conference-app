c4d75bd33084da251e735216f3b7551c8f8c096f
gongwho <hklee4000@gmail.com>
2022-01-26 18:31:55 +0900

    create models for ui in domain.model package

---
+++ app/src/main/java/com/survivalcoding/ifkakao/domain/model/SessionItem.kt
@@ -0,0 +1,18 @@
+package com.survivalcoding.ifkakao.domain.model
+
+data class SessionItem(
+    val idx: Int,
+    val title: String,
+    val content: String,
+    val company: String,
+    val field: String,
+    val contentTag: List<String>,
+    val videoUrl: String, // from LinkList.video.url
+    val videoDuration: String, // from LinkList.video.description
+    val mainImageUrl: String, // from LinkList.mo_main_image
+    val imageUrl: String, // from LinkList.mo_image
+    val classifications: List<String>, // from RelationList.classification
+    val techClassifications: List<String>, // from RelationList.tech_classification
+    val mainExposureDay: Int, // from relationList.main_exposure_day
+    val speakerProfiles: List<SpeakerProfileItem> // from LinkList.SpeakerProfile & contentSpeakerList
+)
