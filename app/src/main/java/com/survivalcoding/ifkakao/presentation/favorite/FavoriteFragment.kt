51d14e55fb9e7193f7a0a72c7ced8fafb9e01c8f
jionchu <fn0809@gmail.com>
2022-02-10 10:06:12 +0900

    좋아요 세션 화면 구현

---
+++ app/src/main/java/com/survivalcoding/ifkakao/presentation/favorite/FavoriteFragment.kt
@@ -0,0 +1,63 @@
+package com.survivalcoding.ifkakao.presentation.favorite
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
+import androidx.fragment.app.viewModels
+import com.survivalcoding.ifkakao.R
+import com.survivalcoding.ifkakao.databinding.FragmentFavoriteBinding
+import com.survivalcoding.ifkakao.domain.model.Session
+import com.survivalcoding.ifkakao.presentation.adapter.SessionListAdapter
+import com.survivalcoding.ifkakao.presentation.detail.DetailFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class FavoriteFragment : Fragment() {
+    private var _binding: FragmentFavoriteBinding? = null
+    private val binding get() = _binding!!
+    private val viewModel: FavoriteViewModel by viewModels()
+    private val adapter by lazy {
+        SessionListAdapter { session -> moveToDetail(session) }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentFavoriteBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // recyclerView 설정
+        binding.favoriteRecyclerView.adapter = adapter
+
+        viewModel.sessions.observe(this) { sessions ->
+            adapter.submitList(sessions)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun moveToDetail(session: Session?) {
+        parentFragmentManager.commit {
+            replace<DetailFragment>(
+                R.id.fragment_container_view,
+                args = bundleOf("sessionId" to session?.idx),
+            )
+            setReorderingAllowed(true)
+            addToBackStack(null)
+        }
+    }
+}
